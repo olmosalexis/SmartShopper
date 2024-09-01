4a8adbdf1ba24df00ab5fb24fdd1605cd79a48ca
olmosalexis <olmoslexis@gmail.com>
2024-08-31 19:17:59 -0700

    Fix Server Component issue by switching to useRouter from next/navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 type FormData = {
   groceryList: string;
